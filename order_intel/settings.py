34b8b1dab20d31483d18bfb707015551ddc81f45
Sumaira <sumaira@pigeontech.io>
2025-12-05 19:31:14 +0500

    push all

--- order_intel/settings.py
+++ order_intel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uhfzm4pcj(#4aoy-!ya(74^57e@*b@nz-w0r*km0dwemgn3#x&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
