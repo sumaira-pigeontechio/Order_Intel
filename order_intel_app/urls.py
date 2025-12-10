ee4f73eb64a60bad971364800fc0156d14027598
Sumaira <sumaira@pigeontech.io>
2025-12-10 15:19:46 +0500

    push all

--- order_intel_app/urls.py
+++ order_intel_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('send-mail/', views.send_mail_view, name="send_mail"),
 ]
--- order_intel_app/views.py
+++ order_intel_app/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.contrib import messages
 
 def home(request):
     return render(request, "base.html")
+
+def send_mail_view(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+
+        full_message = f"""
+        New Contact Message
+
+        Name: {name}
+        Email: {email}
+        Subject: {subject}
+        
+        Message:
+        {message}
+        """
+
+        send_mail(
+            subject,
+            full_message,
+            email,  # from email
+            ['yourgmail@gmail.com'],  # to email
+        )
+
+        messages.success(request, "Your message has been sent successfully!")
+        return redirect("home")
+
+    return redirect("home")
